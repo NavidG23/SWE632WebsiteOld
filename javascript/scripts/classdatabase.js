4bf0f3eadff15f6795f681ea5b7d248ba4e63cb3
NavidG23 <navd.galt@gmail.com>
2018-04-18 12:00:43 -0400

    Updated HW5 old to HW6

--- javascript/scripts/classdatabase.js
+++ javascript/scripts/classdatabase.js
@@ -1,58 +1,64 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //Append the subjects to the subject list.
-    for(i in subject){
-        $(".subjects").append("<li class='subjectListing searchItems' value='" + subject[i].id + "' abbrev='" + subject[i].abbreviation + "' </li><a href='#'>"+ subject[i].name +" ["+ subject[i].abbreviation + "]</a>");
-    } 
-    
+    for (i in subject) {
+        $(".subjects").append("<li class='subjectListing searchItems' value='" + subject[i].id + "' abbrev='" + subject[i].abbreviation + "' </li><a href='#'>" + subject[i].name + " [" + subject[i].abbreviation + "]</a>");
+    }
+
     /**
      * Handler for select dropdown change.
      */
-    $('#term').change(function(){
+    $('#term').change(function () {
         $('#subjectSection').show();
-        $("#coursesSection").hide(); 
-        $("#coursesSelectionSection").hide();        
+        $("#coursesSection").hide();
+        $("#coursesSelectionSection").hide();
     })
 
     /**
      * Append courses when subject is clicked.
      */
-    $('.subjectListing').on('click',function(e){
+    $('.subjectListing').on('click', function (e) {
         e.preventDefault(); //Page won't jump to top
         var courses = getCoursesForSubject($(this).val());
         var subjectAbbrevation = $(this).attr('abbrev');
         $('#subjectSection').hide();
-        $("#coursesSection").show();        
+        $("#coursesSection").show();
         $(".courses").empty();
         //Add courses to the list
-        if(courses.length == 0){
+        if (courses.length == 0) {
             //No courses available
             $(".courses").append("<li class='courseListing'><a>There are currently no courses available.</a></li>");
-        }else{
-            for(i in courses){
-                $(".courses").append("<li class='courseListing searchItems' value='"+ courses[i].id + "' </li><a href='#' class='courseListingLink'>"+ subjectAbbrevation + " " + courses[i].name +"</a>");
+        } else {
+            for (i in courses) {
+                if (courses[i].name.includes("Already Taken")) {
+                    $(".courses").append("<li class='courseListing searchItems' value='" + courses[i].id +
+                        "' </li><a href='#' class='courseListingLink disabled'>" + subjectAbbrevation + " " + courses[i].name + "</a>");
+                } else {
+                    $(".courses").append("<li class='courseListing searchItems' value='" + courses[i].id +
+                        "' </li><a href='#' class='courseListingLink'>" + subjectAbbrevation + " " + courses[i].name + "</a>");
+                }
             }
         }
         activateCourseListeners(); //Activate handlers for newly created course items.    
-        $('#myInput').attr("placeholder", "Search for Course...");     
+        $('#myInput').attr("placeholder", "Search for Course...");
     });
 
     /**
      * Empties course and section data, returns back to original subject list.
      */
-    $('#backToSubjects').on('click',function(e){
+    $('#backToSubjects').on('click', function (e) {
         e.preventDefault();
         $('.sections').empty();
         $(".courses").empty();
         $("#coursesSection").hide();
         $('#subjectSection').show();
-        $('#myInput').val('').trigger('input');        
-        $('#myInput').attr("placeholder", "Search for Subject...");     
+        $('#myInput').val('').trigger('input');
+        $('#myInput').attr("placeholder", "Search for Subject...");
     })
 
     /**
      * Activate filter bar
      */
-    $('#myInput').on('input', function(){
+    $('#myInput').on('input', function () {
         search($(this));
     });
 });
@@ -61,11 +67,11 @@ $(document).ready(function(){
  * Activate click handlers for course listings. Done after they are dynamically rendered
  * when a user selects a subject
  */
-function activateCourseListeners(){
-    $('.courseListing').on('click', function(e){
+function activateCourseListeners() {
+    $('.courseListing').on('click', function (e) {
         e.preventDefault(); //Page won't jump to top
         //Remove highlights from all other courses
-        $('.courseListingLink').each(function(){
+        $('.courseListingLink').each(function () {
             $(this).removeClass('highlight');
         })
         //Highlight the course selected
@@ -73,25 +79,61 @@ function activateCourseListeners(){
         //Get the sections available by the course and term ids. Method located in register.js
         var sections = getSectionsByCourseAndTerm(parseInt($('#term').val()), $(this).val())
         $('#subjectSection').hide();
-        $("#coursesSection").show();   
+        $("#coursesSection").show();
         $("#coursesSelectionSection").show();
         $(".sections").empty();
 
         //Add sections to list
-        if(sections.length == 0){
+        if (sections.length == 0) {
             //No sections available
             $(".sections").append("<li class='sectionsListing'><a>There are currently no sections available.</a></li>");
-        }else{
+        } else {
             //For Table (header)
-            $(".sections").append("<tr><th>Day</th><th>Time</th><th>Instructor</th><th>Location</th><th>Rem Seats</th></tr>");
+            $(".sections").append("<tr><th>Day</th><th>Time</th><th>Instructor</th><th>Location</th><th>Rem Seats</th><th></th></tr>");
             //Go through selected sections and append them to the list.
-            for(i in sections){
+            for (i in sections) {
                 //For Table (rows)
-                $(".sections").append("<tr><td>" + sections[i].days + "</td><td>" + sections[i].timeslot + "</td><td>" + sections[i].teacher + "</td><td>" + sections[i].location + "</td><td>" + sections[i].remainingseats + "</td></tr>");
-                //For List 
+                $(".sections").append("<tr><td>" + sections[i].days +
+                    "</td><td>" + sections[i].timeslot + "</td><td>" + sections[i].teacher + "</td><td>" +
+                    sections[i].location + "</td><td>" + sections[i].remainingseats +
+                    "</td><td><button class='copy register_button' data-key=" + (i + 1) + ">Copy</button></td></tr>");
+
+                $('button.copy').on('click', function () {
+                    var cRow = $(this).parents('tr');
+                    var selectedCourse = getCourseById(sections[i].courseId);
+                    var selectedSubject = getSubjectById(selectedCourse.subjectId);
+                    var day = $('td:nth-child(1)', cRow).text();
+                    var time = $('td:nth-child(2)', cRow).text();
+                    var instructor = $('td:nth-child(3)', cRow).text();
+                    var location = $('td:nth-child(4)', cRow).text();
+                    var intKey = $(this).data('key');
+
+                    doCopy(selectedCourse.name, selectedSubject, day, time, instructor, location, intKey);
+                });
+
+                function doCopy(selectedCourse, selectedSubject, day, time, instructor, location, key) {
+                    copyToClipboard("Subject: " + selectedSubject + " " + selectedCourse + "\n"
+                        + "Day of Week: " + day + "\n"
+                        + "Time of Day: " + time + "\n"
+                        + "Instructor: " + instructor + "\n"
+                        + "Location: " + location);
+                }
+
+                function copyToClipboard(element) {
+                    const el = document.createElement('textarea');
+                    el.value = element;
+                    document.body.appendChild(el);
+                    el.select();
+                    document.execCommand('copy');
+                    document.body.removeChild(el);
+
+                    alert("Successfully copied to the clipboard");
+                }
+
+                //For List
                 //$(".sections").append("<li class='sectionsListing' value='"+ sections[i].id + "' </li><a href='#'>"+ " " + sections[i].days + " | " + sections[i].timeslot + " | " + sections[i].teacher + " | " + sections[i].location + " | " + sections[i].remainingseats + "</a>");
-            } 
-        }         
+            }
+        }
     });
 }
 
@@ -99,15 +141,15 @@ function activateCourseListeners(){
  * Allows for filter capability on subjects and courses
  * @param {String} obj - The filter text
  */
-function search(obj){    
+function search(obj) {
     var filter = obj.val().toUpperCase();
     var items = $('li.searchItems');
     //Go through each item, see if it contains filter text
-    $.each(items, function(){
+    $.each(items, function () {
         var text = $(this).text().toUpperCase();
-        if(text.indexOf(filter) > -1) {
+        if (text.indexOf(filter) > -1) {
             $(this).show();
-        }else{
+        } else {
             $(this).hide();
         }
     });
--- javascript/scripts/register.js
+++ javascript/scripts/register.js
@@ -49,6 +49,25 @@ function getSectionsByCourseAndTerm(termId, courseId){
     return sections;
 }
 
+function getCourseById(courseId) {
+    var courseName = "";
+    for (i in course) {
+        if (course[i].id == courseId) {
+            courseName = course[i];
+        }
+    }
+    return courseName;
+}
+
+function getSubjectById(subjectId) {
+    var subjectName = "";
+    for (i in subject) {
+        if (subject[i].id == subjectId) {
+            subjectName = subject[i].abbreviation;
+        }
+    }
+    return subjectName;
+}
 
 
 /**** JSON MOCK DATA *****/
@@ -106,179 +125,179 @@ var subjectTerm = [
 var course = [
         {
             "id": 1,
-            "name": "205- Software Usability Analysis and Design",
+            "name": "205 - Software Usability Analysis and Design",
             "code": 205,
             "subjectId": "1"
         },
         {
             "id": 2,
-            "name": "301- Internship Preparation",
+            "name": "301 - Internship Preparation",
             "code": 301,
             "subjectId": "1"
 
         },
         {
             "id": 3,
-            "name": "401- Internship Reflection",
+            "name": "401 - Internship Reflection",
             "code": 401,
             "subjectId": "1"
         },
         {
             "id": 4,
-            "name": "437- Software Testing and Maintenance",
+            "name": "437 - Software Testing and Maintenance",
             "code": 437,
             "subjectId": "1"
         },
         {
             "id": 5,
-            "name": "510- Object-Oriented Programming in Java",
+            "name": "510 - Object-Oriented Programming in Java",
             "code": 510,
             "subjectId": "1"
         },
         {
             "id": 6,
-            "name": "619- Object-Oriented Software Specification and Construction",
+            "name": "619 - Object-Oriented Software Specification and Construction<br>(Already Taken)",
             "code": 619,
             "subjectId": "1"
         },
         {
             "id": 7,
-            "name": "621- Software Modeling and Architectural Design",
+            "name": "621 - Software Modeling and Architectural Design<br>(Already Taken)",
             "code": 621,
             "subjectId": "1"
         },
         {
             "id": 8,
-            "name": "622- Distributed Software Engineering",
+            "name": "622 - Distributed Software Engineering<br>(Already Taken)",
             "code": 622,
             "subjectId": "1"
         },
         {
             "id": 9,
-            "name": "625- Software Project Management",
+            "name": "625 - Software Project Management",
             "code": 625,
             "subjectId": "1"
 
         },
         {
             "id": 10,
-            "name": "632- User Interface and Design",
+            "name": "632 - User Interface and Design<br>(Already Taken)",
             "code": 632,
             "subjectId": "1"
         },
         {
             "id": 11,
-            "name": "637- Software Testing",
+            "name": "637 - Software Testing<br>(Already Taken)",
             "code": 637,
             "subjectId": "1"
         },
         {
             "id": 12,
-            "name": "642- Software Engineering for WWW",
+            "name": "642 - Software Engineering for WWW<br>(Already Taken)",
             "code": 642,
             "subjectId": "1"
         },
         {
             "id": 13,
-            "name": "645- Component-Based Software Development",
+            "name": "645 - Component-Based Software Development",
             "code": 645,
             "subjectId": "1"
         },
         {
             "id": 14,
-            "name": "681- Secure Software Design and Programming",
+            "name": "681 - Secure Software Design and Programming",
             "code": 681,
             "subjectId": "1"
         },
         {
             "id": 15,
-            "name": "737- Advanced Software Testing",
+            "name": "737 - Advanced Software Testing",
             "code": 737,
             "subjectId": "1"
         },
         {
             "id": 16,
-            "name": "100- Principles of Computing",
+            "name": "100 - Principles of Computing",
             "code": 100,
             "subjectId": "2"
         },
         {
             "id": 17,
-            "name": "105- Computer Ethics and Society",
+            "name": "105 - Computer Ethics and Society",
             "code": 105,
             "subjectId": "2"
 
         },
         {
             "id": 18,
-            "name": "110- Essentials of Computer Science",
+            "name": "110 - Essentials of Computer Science",
             "code": 110,
             "subjectId": "2"
         },
         {
             "id": 19,
-            "name": "112- Intro Computer Programming",
+            "name": "112 - Intro Computer Programming",
             "code": 112,
             "subjectId": "2"
         },
         {
             "id": 20,
-            "name": "211- Object-Oriented Programming",
+            "name": "211 - Object-Oriented Programming",
             "code": 211,
             "subjectId": "2"
         },
         {
             "id": 21,
-            "name": "262- Intro to Low-Level Programming",
+            "name": "262 - Intro to Low-Level Programming",
             "code": 262,
             "subjectId": "2"
         },
         {
             "id": 22,
-            "name": "306- Synthetic Ethics and Law for Computer Professionals",
+            "name": "306 - Synthetic Ethics and Law for Computer Professionals",
             "code": 306,
             "subjectId": "2"
         },
         {
             "id": 23,
-            "name": "310- Data Structures",
+            "name": "310 - Data Structures",
             "code": 310,
             "subjectId": "2"
         },
         {
             "id": 24,
-            "name": "321- Software Engineering",
+            "name": "321 - Software Engineering",
             "code": 321,
             "subjectId": "2"
 
         },
         {
             "id": 25,
-            "name": "325- Introduction to Game Design",
+            "name": "325 - Introduction to Game Design",
             "code": 325,
             "subjectId": "2"
         },
         {
             "id": 26,
-            "name": "330- Formal Methods and Models",
+            "name": "330 - Formal Methods and Models",
             "code": 330,
             "subjectId": "2"
         },
         {
             "id": 27,
-            "name": "351- Visual Computing",
+            "name": "351 - Visual Computing",
             "code": 351,
             "subjectId": "2"
         },
         {
             "id": 28,
-            "name": "367- Computer Systems and Programming",
+            "name": "367 - Computer Systems and Programming",
             "code": 367,
             "subjectId": "2"
         },
         {
             "id": 29,
-            "name": "426- Game Programming II",
+            "name": "426 - Game Programming II",
             "code": 426,
             "subjectId": "2"
         },
@@ -290,75 +309,75 @@ var course = [
         },
         {
             "id": 31,
-            "name": "515- Computer Organization and Operating Systems",
+            "name": "515 - Computer Organization and Operating Systems",
             "code": 515,
             "subjectId": "4"
         },
         {
             "id": 32,
-            "name": "519- Program Design and Data Structures",
+            "name": "519 - Program Design and Data Structures",
             "code": 519,
             "subjectId": "4"
 
         },
         {
             "id": 33,
-            "name": "612- Principles and Practice of Communication Networks",
+            "name": "612 - Principles and Practice of Communication Networks",
             "code": 612,
             "subjectId": "4"
         },
         {
             "id": 34,
-            "name": "622- Information Systems Analysis and Design",
+            "name": "622 - Information Systems Analysis and Design",
             "code": 6222,
             "subjectId": "4"
         },
         {
             "id": 35,
-            "name": "774- Enterprise Architecture",
+            "name": "774 - Enterprise Architecture",
             "code": 774,
             "subjectId": "4"
         },
         {
             "id": 36,
-            "name": "562- Information Security Theory and Practice",
+            "name": "562 - Information Security Theory and Practice",
             "code": 562,
             "subjectId": "3"
         },
         {
             "id": 37,
-            "name": "564- Security Laboratory",
+            "name": "564 - Security Laboratory",
             "code": 564,
             "subjectId": "3"
         },
         {
             "id": 38,
-            "name": "650- Security Policy",
+            "name": "650 - Security Policy",
             "code": 650,
             "subjectId": "3"
         },
         {
             "id": 39,
-            "name": "656- Network Security",
+            "name": "656 - Network Security",
             "code": 656, 
             "subjectId": "3"
 
         },
         {
             "id": 40,
-            "name": "674- Intrusion Detection",
+            "name": "674 - Intrusion Detection",
             "code": 674,
             "subjectId": "3"
         },
         {
             "id": 41,
-            "name": "681- Secure Software Design/Programming",
+            "name": "681 - Secure Software Design/Programming",
             "code": 681,
             "subjectId": "3"
         },
         {
             "id": 42,
-            "name": "763- Secure Protocol Analysis",
+            "name": "763 - Secure Protocol Analysis",
             "code": 763,
             "subjectId": "3"
         },    
